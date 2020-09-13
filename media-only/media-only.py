5b84aa0e443ceae4ebc261a9328143d465bbff7d
lorenzo132 <50767078+lorenzo132@users.noreply.github.com>
2020-09-13 15:18:56 +0200

    Update media-only.py

--- media-only/media-only.py
+++ media-only/media-only.py
@@ -7,7 +7,7 @@ from core import checks
 from core.models import PermissionLevel
 
 
-class mediaonly(commands.Cog):
+class Mediaonly(commands.Cog):
     """Sets up media channel in discord."""
 
     def __init__(self, bot):
@@ -107,4 +107,4 @@ class mediaonly(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(mediaonly(bot))
+    bot.add_cog(Mediaonly(bot))
