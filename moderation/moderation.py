40c25c588bf0be8583e09aed42e1a741fee5a3c0
lorenzo132 <50767078+lorenzo132@users.noreply.github.com>
2020-09-13 16:26:40 +0200

    Update moderation.py

--- moderation/moderation.py
+++ moderation/moderation.py
@@ -202,27 +202,23 @@ class moderation(commands.Cog):
                 color = self.errorcolor
             )
             await ctx.send(embed = embed, delete_after = 5.0)
+
     #massban command
     @commands.command()
-    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
-    async def massban(self, ctx, *, ids:str):
-		await ctx.channel.trigger_typing()
-		ids = ids.split(" ")
-		guild = ctx.guild
-		failed_ids = []
-		success = 0
-		for id in ids:
-			try:
-				await self.bot.http.ban(int(id), ctx.guild.id, delete_message_days=0)
-				success += 1
-			except:
-				failed_ids.append("`{}`".format(id))
-		if len(failed_ids) != 0:
-			await ctx.send(Language.get("moderation.massban_failed_ids", ctx).format(", ".join(ids)))
-		await ctx.send(Language.get("moderation.massban_success", ctx).format(success, len(ids)))
-		for channel in guild.channels:
-			if channel.name == "logs":
-				await channel.send(Language.get("moderation.massban_success", ctx).format(success, len(ids)))
+    @commands.guild_only()
+    @commands.max_concurrency(1, per=commands.BucketType.user)
+    @permissions.has_permissions(ban_members=True)
+    async def massban(self, ctx, reason: ActionReason, *members: MemberID):
+        """ Mass bans multiple members from the server. """
+        try:
+            for member_id in members:
+                await ctx.guild.ban(
+                    discord.Object(id=member_id),
+                    reason=default.responsible(ctx.author, reason),
+                )
+            await ctx.send(default.actionmessage("massbanned", mass=True))
+        except Exception as e:
+            await ctx.send(e)
 
     #Unban command
     @commands.command()
