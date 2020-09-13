da8390b9a0d92692175bbdd0ea56c1d2a8c11278
lorenzo132 <50767078+lorenzo132@users.noreply.github.com>
2020-09-13 15:18:04 +0200

    Update media-only.py

--- media-only/media-only.py
+++ media-only/media-only.py
@@ -7,8 +7,8 @@ from core import checks
 from core.models import PermissionLevel
 
 
-class Mediaonly(commands.Cog):
-    """Sets up emoji suggestor channel in Modmail discord."""
+class mediaonly(commands.Cog):
+    """Sets up media channel in discord."""
 
     def __init__(self, bot):
         self.bot = bot
@@ -107,4 +107,4 @@ class Mediaonly(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(Mediaonly(bot))
+    bot.add_cog(mediaonly(bot))
