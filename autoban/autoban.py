63e7f7e9123b2b88e7170861bab72a3cf836f9aa
lorenzo132 <50767078+lorenzo132@users.noreply.github.com>
2021-04-04 21:04:53 +0200

    Update autoban.py

--- autoban/autoban.py
+++ autoban/autoban.py
@@ -4,12 +4,14 @@ from discord.ext import commands
 class AutoBan(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.bans = [767398969147392030, 531602381591543818]
+        self.bans = [123, 456, 789]
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
         guild = member.guild
         if member.id in self.bans:
+            embed = discord.Embed(title="AutoBan", description="You have been AutoBanned in **{guild.name}**", color=self.bot.main_color)
+            await member.send(embed=embed)
             await guild.ban(member, reason="AutoBanned due to AutoBan List")
 
 def setup(bot):
