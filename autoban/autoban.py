f606f668ce46d54eac638600c2f515be353b9b6c
lorenzo132 <50767078+lorenzo132@users.noreply.github.com>
2021-04-04 20:31:25 +0200

    Update autoban.py

--- autoban/autoban.py
+++ autoban/autoban.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 class AutoBan(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.bans = [617711889169776641, 188363246695219201]
+        self.bans = [1212, 1212]
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
