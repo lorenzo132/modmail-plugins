2fbfc97d19588734f53941f0cf7415ac05a60412
lorenzo132 <50767078+lorenzo132@users.noreply.github.com>
2021-04-05 23:15:02 +0200

    Update media-logger.py

--- media-logger/media-logger.py
+++ media-logger/media-logger.py
@@ -31,7 +31,7 @@ class MediaLogger(commands.Cog):
         em.set_author(name=m.author.name, icon_url=m.author.avatar_url)
         em.set_footer(text=f'U: {m.author.id} | C: {m.channel.id} | M: {m.id}')
         for a in m.attachments:
-            if a.filename.endswith('.png') or a.filename.endswith('.jpeg') or a.filename.endswith('.gif') or a.filename.endswith('.jpg'):
+            if a.filename.endswith('.png') or a.filename.endswith('.jpeg') or a.filename.endswith('.gif') or a.filename.endswith('.mp4') or a.filename.endswith('.jpg'):
                 file = await a.to_file()
                 channel = await self.log_channel()
                 if channel:
