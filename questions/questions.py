5bb53f595a7eae74f83396efcb1d03d5531d8e44
lorenzo132 <50767078+lorenzo132@users.noreply.github.com>
2022-06-05 16:05:32 +0200

    Revert "test fix img bug"
    This reverts commit 4ed7271771554ddc82ed289e6ea5d7566e64f853.

--- questions/questions.py
+++ questions/questions.py
@@ -39,13 +39,7 @@ class Questions(commands.Cog):
                 await thread.close(closer=self.bot.modmail_guild.me, message='Closed due to inactivity and not responding to questions')
                 return
             else:
-                answer = m.content if message.content or "<No Message Content>"
-                answer += "\n"
-                if len(m.attachments) > 0:
-                    for attachment in m.attachments:
-                        answer += f"\n`{attachment.filename}`: {attachment.url}"
-
-                responses[question] = answer
+                responses[question] = m.content
 
         await asyncio.sleep(1)
         em = discord.Embed(color=self.bot.main_color, timestamp=datetime.utcnow())
