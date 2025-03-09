673838abbfe903954a73cccb69d6ef91d12aaaa8
lorenzo132 <50767078+lorenzo132@users.noreply.github.com>
2025-03-09 13:36:40 +0100

    Update topsupporters.py

--- topsupporters/topsupporters.py
+++ topsupporters/topsupporters.py
@@ -19,47 +19,35 @@ class TopSupporters(commands.Cog):
     async def topsupporters(self, ctx, *, dt: UserFriendlyTime):
         """Retrieves top supporters for the specified time period"""
         async with ctx.typing():
-            # Calculate the date boundary
             date = discord.utils.utcnow() - (dt.dt - discord.utils.utcnow())
 
-            # Retrieve logs where tickets are closed
             logs = await self.bot.api.logs.find({"open": False}).to_list(None)
             logs = filter(lambda x: isinstance(x['closed_at'], str) and datetime.fromisoformat(x['closed_at']) > date, logs)
 
-            # Dictionary to store supporter counts
             supporters = defaultdict(int)
 
-            for log in logs:
+            for l in logs:
                 supporters_involved = set()
-                for message in log['messages']:
-                    # Check if the message was sent by a mod and is either anonymous or a thread message
-                    if message.get('type') in ('anonymous', 'thread_message') and message['author'].get('mod', False):
-                        supporters_involved.add(message['author']['id'])
-                # Count supporters
-                for supporter in supporters_involved:
-                    supporters[supporter] += 1
+                for x in l['messages']:
+                    if x.get('type') in ('anonymous', 'thread_message') and x['author']['mod']:
+                        supporters_involved.add(x['author']['id'])
+                for s in supporters_involved:
+                    supporters[s] += 1
 
-            # Sort supporters by count in descending order
             supporters_keys = sorted(supporters.keys(), key=lambda x: supporters[x], reverse=True)
 
-            # Build the embed description
             fmt = ''
+
             n = 1
-            for supporter_id in supporters_keys:
-                user = self.bot.get_user(supporter_id)
-                if user:
-                    fmt += f'**{n}.** `{user}` - {supporters[supporter_id]}\n'
+            for k in supporters_keys:
+                u = self.bot.get_user(int(k))
+                if u:
+                    fmt += f'**{n}.** `{u}` - {supporters[k]}\n'
                     n += 1
 
-            # Create embed message
-            embed = discord.Embed(
-                title='Top Supporters',
-                description=fmt or "No supporters found for the specified time period.",
-                timestamp=datetime.utcnow(),
-                color=0x7588da
-            )
-            embed.set_footer(text=f'Since {date.strftime("%Y-%m-%d %H:%M:%S")}')
-            await ctx.send(embed=embed)
+            em = discord.Embed(title='Active Supporters', description=fmt, timestamp=date, color=0x7588da)
+            em.set_footer(text='Since')
+            await ctx.send(embed=em)
 
 
 async def setup(bot):
