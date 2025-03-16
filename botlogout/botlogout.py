ac6723e0ffd1f560275d233bd6d0bf4ef5b911c4
lorenzo132 <50767078+lorenzo132@users.noreply.github.com>
2025-03-16 16:45:04 +0100

    Update botlogout.py

--- botlogout/botlogout.py
+++ botlogout/botlogout.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+from core import checks
+from core.checks import PermissionLevel
 
 class BotLogout(commands.Cog):
     def __init__(self, bot):
@@ -7,7 +9,8 @@ class BotLogout(commands.Cog):
 
     @checks.has_permissions(PermissionLevel.OWNER)
     @commands.command()
-    async def botlogout(self,__):
+    async def botlogout(self, _):
+        """Logs out the bot"""
         await self.bot.close()
 
 async def setup(bot):
