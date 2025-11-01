c7352eee28939d495c9b0edcfc8430a392d966a2
lorenzo132 <lhoorn4@gmail.com>
2025-11-01 22:56:15 +0100

    Create guildcleanup.py

---
+++ guildcleanup-2/guildcleanup.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands, tasks
+
+
+WHITELISTED_GUILD_IDS = {1267806213656023071}
+
+
+class GuildCleanup(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.cleanup_guilds.start()
+
+    def cog_unload(self):
+        self.cleanup_guilds.cancel()
+
+    @tasks.loop(minutes=2)
+    async def cleanup_guilds(self):
+        for guild in self.bot.guilds:
+            if guild.id not in WHITELISTED_GUILD_IDS:
+                try:
+                    await guild.leave()
+                    print(f"Left guild: {guild.name} ({guild.id})")
+                except discord.HTTPException as e:
+                    print(f"Failed to leave guild {guild.id}: {e}")
+
+    @cleanup_guilds.before_loop
+    async def before_cleanup(self):
+        await self.bot.wait_until_ready()
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(GuildCleanup(bot))
